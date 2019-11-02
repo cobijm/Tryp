544599f5cab4d79d07cbeeaed199d6b288090ddc
Ismael <duran.ismael30@gmail.com>
2019-11-02 02:28:44 -0500

    event image

--- reactApp/webapp/src/App.js
+++ reactApp/webapp/src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+
+
+class App extends React.Component {
+  state = {
+    imgurl : undefined,
+    
+  }
+
+    componentDidMount = async(e) => {
+      const url = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=mLGxhaIVykD4qeMZWvIi8lmIoMf7RAP7&latlong=42.0307812,-93.63191309999999";
+      const response = await fetch(url);
+      const data = await response.json();
+      var randomEvent =Math.floor(Math.random()*(20 - 0));
+      console.log(randomEvent);
+      this.setState({
+        imgurl: data._embedded.events[randomEvent].images[0].url
+      }
+      )
+      console.log(data);
+    }
+
+
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> <img src={this.state.imgurl} alt="Error"/>
     </div>
   );
 }
+}
+
+
 
 export default App;
